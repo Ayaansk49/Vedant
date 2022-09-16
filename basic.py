bd502f8fcca857fa9535428923fde102061bed38
Ayaansk49 <113667254+Ayaansk49@users.noreply.github.com>
2022-09-16 12:58:29 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,8 @@
+num1=input(int("Enter the 1st number))
+num2=input(int("Enter the 2st number))
+               
+sum = num1+num2
+               
+printf("The answere is {0}".format(sum))
+       
+               
